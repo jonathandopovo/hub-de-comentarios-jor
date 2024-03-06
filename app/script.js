d3a71a7a311bdc3d278cf069773f7555b606819c
0000723669 <0000723669@senaimgaluno.com.br>
2024-03-05 22:12:10 -0300

    Adicionando comentário

--- app/script.js
+++ app/script.js
@@ -1,3 +1,31 @@
+let data = [];
+
 const submitComment = (e) => {
   e.preventDefault();
+  const author = inputAuthor.value;
+  const comment = inputComment.value;
+
+  data.push({ author, comment });
+  loadComment();
 };
+
+const loadComment = () => {
+  if (data) {
+    displayComment();
+  }
+};
+
+const displayComment = () => {
+  const body = document.getElementById("form-comentario");
+  data.forEach((element) => {
+    const divDisplay = document.createElement("div");
+    divDisplay.innerHTML = `
+    <strong>${element.author}</strong>
+    <p>${element.comment}</p>
+    `;
+    body.appendChild(divDisplay);
+  });
+};
+
+const formComment = document.getElementById("formComment");
+formComment.addEventListener("submit", submitComment);
